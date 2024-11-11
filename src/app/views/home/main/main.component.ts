636836d23558d6ff60b3fbc5e7b544aada748dd6
Farid Gasimov <faridgasimov.93@gmail.com>
2024-11-11 23:11:17 +0400

    comented popup

--- src/app/views/home/main/main.component.ts
+++ src/app/views/home/main/main.component.ts
@@ -78,7 +78,7 @@ export class MainComponent implements OnInit, OnDestroy, AfterViewInit {
   private popupComponent!: PopupComponent;
 
   ngAfterViewInit() {
-    this.popupComponent.open();
+    // this.popupComponent.open();
   }
 
   ngOnDestroy() {
